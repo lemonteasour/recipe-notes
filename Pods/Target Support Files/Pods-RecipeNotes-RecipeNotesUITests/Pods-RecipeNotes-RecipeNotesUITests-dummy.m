f565224710ae93bf63113c63f11a786c71e034ad
jay <29498758+jayhuich@users.noreply.github.com>
2025-11-10 12:56:26 +0900

    build: Deintegrate CocoaPods

--- Pods/Target Support Files/Pods-RecipeNotes-RecipeNotesUITests/Pods-RecipeNotes-RecipeNotesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RecipeNotes_RecipeNotesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RecipeNotes_RecipeNotesUITests
-@end
--- Pods/Target Support Files/Pods-RecipeNotesTests/Pods-RecipeNotesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RecipeNotesTests : NSObject
-@end
-@implementation PodsDummy_Pods_RecipeNotesTests
-@end
